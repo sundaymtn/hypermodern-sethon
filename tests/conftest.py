2d0a4edf2f3784605fbe205602f0a0ae31cbe3df
Seth Carter <seth.carter@cd-adapco.com>
2022-05-27 11:10:55 -0400

    chapter 2

---
+++ tests/conftest.py
@@ -0,0 +1,22 @@
+import pytest
+import click
+
+def pytest_configure(config):
+    config.addinivalue_line("markers", "e2e: mark as end-to-end test.")
+
+@pytest.fixture
+def mock_requests_get(mocker):
+    mock = mocker.patch("requests.get")
+    mock.return_value.__enter__.return_value.json.return_value = {
+        "title": "Lorem Ipsum",
+        "extract": "Lorem ipsum dolor sit amet",
+    }
+    return mock
+
+@pytest.fixture
+def runner():
+    return click.testing.CliRunner()
+
+@pytest.fixture
+def mock_wikipedia_random_page(mocker):
+    return mocker.patch("hypermodern_sethon.wikipedia.random_page")
